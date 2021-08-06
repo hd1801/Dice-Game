3bb573e3c339688245719cefb7090c6d79003703
hd1801 <hardikdhawan0108@gmail.com>
2021-08-07 00:45:51 +0530

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,15 @@
-let randNum1= Math.floor(Math.random()*6+1);
-let randNum2= Math.floor(Math.random()*6+1);
-document.querySelectorAll(".dice img")[0].setAttribute("src","/assets/images/dice"+randNum1+".png");
-document.querySelectorAll(".dice img")[1].setAttribute("src","/assets/images/dice"+randNum2+".png");
-if(randNum2>randNum2){
-    
+function throwDice(){
+  let randNum1= Math.floor(Math.random()*6+1);
+  let randNum2= Math.floor(Math.random()*6+1);
+  document.querySelectorAll(".dice img")[0].setAttribute("src","/assets/images/dice"+randNum1+".png");
+  document.querySelectorAll(".dice img")[1].setAttribute("src","/assets/images/dice"+randNum2+".png");
+  if(randNum2>randNum1){  
+    document.querySelector(".container h1").textContent="PLAYER 2 WINS!!";
+  } else if (randNum2===randNum1) {  
+      document.querySelector(".container h1").textContent="ITS A DRAW";
+  } else {
+      document.querySelector(".container h1").textContent="PLAYER 1 WINS!!";
+  }
 }
+
+document.querySelector(".btn").addEventListener("click",throwDice);
