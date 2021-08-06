1c0e88fb0b8d8a67ae00c9bf395e5532d6088e01
hd1801 <hardikdhawan0108@gmail.com>
2021-08-07 00:37:31 +0530

    update

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,7 @@
-let randomNum1=Math.random()*6+1;
-let randomNum2=Math.random()*6+1;
+let randNum1= Math.floor(Math.random()*6+1);
+let randNum2= Math.floor(Math.random()*6+1);
+document.querySelectorAll(".dice img")[0].setAttribute("src","/assets/images/dice"+randNum1+".png");
+document.querySelectorAll(".dice img")[1].setAttribute("src","/assets/images/dice"+randNum2+".png");
+if(randNum2>randNum2){
+    
+}
